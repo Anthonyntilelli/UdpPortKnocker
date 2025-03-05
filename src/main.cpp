7ecd8cc7e24e85a3595b4e762e415f4d28ba238e
Anthony Tilelli <Anthony@TIlelli.me>
2025-03-04 23:25:44 -0500

    added logger and minor other changes (#2)

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "config.h"
+#include "logger.h"
 #include "utility.h"
 #include <cstring>
 
 constexpr auto config_file{"config/udpknocker.conf"};
 
 int main(int argc, char *argv[]) {
+  Config cfg{}; // load before usage
   if (argc < 2) {
     std::cerr << "Missing parameter(s)" << std::endl;
     return 1;
@@ -17,11 +19,21 @@ int main(int argc, char *argv[]) {
       return 1;
     }
     try {
-      Config cfg(argv[2]); // argv[2] is config file path
+      cfg.load(argv[2]); // argv[2] is config file path
     } catch (const std::invalid_argument &e) {
       std::cerr << e.what() << std::endl;
       return 1;
     }
+  } else if (std::strcmp(argv[1], "server") == 0) {
+    if (argc != 2)
+      std::cerr << "To many argument" << std::endl;
+    try {
+      cfg.load(config_file);
+    } catch (const std::invalid_argument &e) {
+      std::cerr << e.what() << std::endl;
+      return 1;
+    }
+    Logger &log = Logger::getInstance(cfg.getLogFile());
   } else {
     std::cerr << "Unknown parameter" << std::endl;
     return 1;
